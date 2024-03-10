1758ff1e1c756f07aadc9c6afd3af7debed2bfb2
Noémie <kaeseno1@students.zhaw.ch>
2024-03-10 19:34:46 +0100

    change web look, connect api & display - still bugs to be fixed

--- api.py
+++ api.py
@@ -25,3 +25,21 @@ if __name__ == "__main__":
     if weather_data:
         print("Weather data:")
         print(weather_data)
+
+
+def get_insight_weather(api_key):
+    base_url = "https://api.nasa.gov/insight_weather/"
+    params = {
+        "api_key": api_key,
+        "feedtype": "json",
+        "ver": "1.0"
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        return weather_data
+    else:
+        print(f"Error: {response.status_code}")
+        return None
--- app.py
+++ app.py
@@ -1,33 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 from flask.helpers import send_file
+from api import get_insight_weather
 
-app = Flask(__name__, static_url_path='/', static_folder='web')
+app = Flask(__name__, static_url_path='/', static_folder='web', template_folder='web')
 
 @app.route("/")
 def indexPage():
-     return send_file("web/index.html")  
+    # API-Schlüssel
+    api_key = "0m4vbWgrfhHHwXv7FS1U51c3TNxlFqZNcNCUuWs2"
+    weather_data = get_insight_weather(api_key)
 
-from markupsafe import escape
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
-
-@app.route("/sum")
-def sum_even():
-    # flask parameters with type and default
-    n = request.args.get('n', default=1, type=int)
-    # logic
-    result = sum([x for x in range(n+1) if x % 2 == 0])
-    # return result as json
-    return jsonify(sum=result)
+    # Wetterdaten an HTML-Seite übergeben
+    return render_template("index.html", weather_data=weather_data)
