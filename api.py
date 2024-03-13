d008395e5df36cab2c89b545581e9e22a675e437
Noémie <kaeseno1@students.zhaw.ch>
2024-03-13 21:10:09 +0100

    changes on API call

--- api.py
+++ api.py
@@ -36,6 +36,17 @@ def get_insight_weather(api_key):
     }
 
     response = requests.get(base_url, params=params)
+    print(response)
+
+    # Extrahiere Temperaturdaten
+    temperature_data = response.get('validity_checks', {}).get('1219', {}).get('AT', {})
+
+    # Überprüfe, ob Temperaturdaten vorhanden und gültig sind
+    if temperature_data.get('valid', False):
+        sol_hours_with_data = temperature_data.get('sol_hours_with_data', [])
+        print(f'Temperature data available for sol hours: {sol_hours_with_data}')
+    else:
+        print('No valid temperature data available.')
 
     if response.status_code == 200:
         weather_data = response.json()
